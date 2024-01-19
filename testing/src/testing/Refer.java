b115ee942bcb5405e6721926524f4e858cec504a
Satwik <Satwik2520@gmail.com>
2024-01-19 17:56:40 +0530

    stash

---
+++ testing/src/testing/Refer.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class Refer {
+
+	public static void main(String[] args) {
+		System.out.println("kdbhvjhdb");
+		
+		
+	}
+	public String nullCheck() {
+		return null;
+	}
+}
